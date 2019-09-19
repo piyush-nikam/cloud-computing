778855f9bdddea18575f98a4098fc30607400e4b
unknown <piyushnikam23@gmail.com>
2019-09-19 07:15:21 +0530

    Completed Application

--- Assignment1_cloudComputing_backend/src/main/java/com/cloud/assignment/controller/FoodAppController.java
+++ Assignment1_cloudComputing_backend/src/main/java/com/cloud/assignment/controller/FoodAppController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cloud.assignment.dao.FoodOrderDAO;
@@ -14,7 +15,7 @@ import com.cloud.assignment.model.Login;
 import com.cloud.assignment.model.OrderEntry;
 import com.cloud.assignment.model.User;
 
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RestController
 public class FoodAppController {
 
@@ -25,41 +26,41 @@ public class FoodAppController {
 	UserDAO udao;
 
 	@PostMapping(path = "/add", consumes = "application/json")
-	public String addFood(@RequestBody OrderEntry entry) {
+	public @ResponseBody String addFood(@RequestBody OrderEntry entry) {
 		FoodOrder fo = new FoodOrder();
 		fo.setUserName(entry.getUserName());
 		fo.setFoodType(entry.getFoodType());
 		fo.setFoodName(entry.getFoodName());
 		fodao.save(fo);
-		return "order processed!";
+		return "{ \"add\": \"order processed!\" }";
 	}
 
 	@GetMapping("/all")
-	public Iterable<FoodOrder> allEntries() {
+	public @ResponseBody Iterable<FoodOrder> allEntries() {
 		return fodao.findAll();
 
 	}
 
 	@PostMapping(path = "/register", consumes = "application/json")
-	public String register(@RequestBody User user) {
+	public @ResponseBody String register(@RequestBody User user) {
 		udao.save(user);
-		return "success";
+		return "{ \"register\": \"success\" }";
 	}
 
 	@PostMapping(path = "/login", consumes = "application/json")
-	public String login(@RequestBody Login login) {
+	public @ResponseBody String login(@RequestBody Login login) {
 		String access = "Not Registered";
 		for (User user : udao.findAll()) {
 			if (user.getUserName().equals(login.getUserName()) && user.getPassword().equals(login.getPassword())) {
-				return "success";
+				access = "success";
+				break;
 			}
 		}
-		return access;
+		return "{ \"auth\": \"" + access + "\" }";
 	}
 
 	@GetMapping("/health")
-	public String checkApp() {
-		return "Application is working";
-
+	public @ResponseBody String checkApp() {
+		return "{ \"health\": \"good\" }";
 	}
 }
